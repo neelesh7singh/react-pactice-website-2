d6029507bfecd4b63662f00ab9b95d026bee9ce6
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-15 12:11:47 +0530

    Updates

---
+++ precache-manifest.63c0deddbb70eaff090d2793476e8dc8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1353aa024d1b862d1fd0d006c4b2c488",
+    "url": "/react-pactice-website-2/index.html"
+  },
+  {
+    "revision": "aa0a5eea492b7855f8ab",
+    "url": "/react-pactice-website-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "467d4e37147973947d62",
+    "url": "/react-pactice-website-2/static/js/2.ace74351.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-pactice-website-2/static/js/2.ace74351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0a5eea492b7855f8ab",
+    "url": "/react-pactice-website-2/static/js/main.4e249310.chunk.js"
+  },
+  {
+    "revision": "1180f85da5a32fae5a2b",
+    "url": "/react-pactice-website-2/static/js/runtime-main.290c731a.js"
+  },
+  {
+    "revision": "fb6ba34d77b5e12a4dcbe1a283177c50",
+    "url": "/react-pactice-website-2/static/media/50-beaches.fb6ba34d.mp4"
+  },
+  {
+    "revision": "cd62565914274c60d51f139d9c43afa2",
+    "url": "/react-pactice-website-2/static/media/easy.cd625659.mp4"
+  },
+  {
+    "revision": "a88de43f12f64eb7d42d7aac7e69b652",
+    "url": "/react-pactice-website-2/static/media/featured-video.a88de43f.mp4"
+  },
+  {
+    "revision": "a797a73047aa34e689c9f0987a280926",
+    "url": "/react-pactice-website-2/static/media/it-takes-an-island.a797a730.mp4"
+  },
+  {
+    "revision": "a2107e500d7f9d76ac8b1ce3a0953764",
+    "url": "/react-pactice-website-2/static/media/make-it-zero.a2107e50.mp4"
+  },
+  {
+    "revision": "0881557a1e415e1836c93cd963293296",
+    "url": "/react-pactice-website-2/static/media/video.0881557a.mp4"
+  }
+]);
